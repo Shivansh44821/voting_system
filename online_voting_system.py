47e13fd04be745bbdf0d130279c112cb25295f4b
Shivansh44821 <146934805+Shivansh44821@users.noreply.github.com>
2025-07-06 13:06:38 +0530

    Update online_voting_system.py

--- online_voting_system.py
+++ online_voting_system.py
@@ -18,7 +18,6 @@ if "vote_verified" not in st.session_state:
 if "verified_user_id" not in st.session_state:
     st.session_state.verified_user_id = None
 
-
 def is_student_registered(student_id):
     cursor.execute("SELECT * FROM students WHERE student_id = %s", (student_id,))
     return cursor.fetchone()
